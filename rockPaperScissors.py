14a651d7d9b6329d2c3d62ba7f69db5acf44c546
jupica <73052273+jupica@users.noreply.github.com>
2022-08-16 09:54:36 +1200

    Add files via upload

---
+++ rockPaperScissors.py
@@ -0,0 +1,46 @@
+import random 
+random_number = random.randint(0,4)
+
+#declare variables
+computer_pick = ''
+user_guess = ''
+
+#computers pick
+if random_number == 0:
+    computer_pick = 'rock'
+elif random_number == 1:
+    computer_pick = 'paper'
+elif random_number == 2:
+    computer_pick = 'scissors'
+elif random_number == 3:
+    computer_pick = 'lizard'
+elif random_number == 4:
+    computer_pick = 'spock'
+
+# set up while loop for valid input
+while (user_guess != 'rock' and 
+       user_guess != 'paper' and 
+       user_guess != 'scissors' and 
+       user_guess != 'lizard' and 
+       user_guess != 'spock'):
+  user_guess = input('rock, paper, scissors, lizard or spock? ').lower()
+
+#if tie
+if computer_pick == user_guess:
+    print('Tie! Both chose ' + computer_pick + ', click run to play again.')
+#if computer wins
+elif ((computer_pick == 'scissors' and user_guess == 'paper') or 
+      (computer_pick == 'paper' and user_guess == 'rock') or 
+      (computer_pick == 'rock' and user_guess == 'lizard') or
+      (computer_pick == 'lizard' and user_guess == 'spock') or
+      (computer_pick == 'spock' and user_guess == 'scissors') or
+      (computer_pick == 'scissors' and user_guess == 'lizard') or
+      (computer_pick == 'lizard' and user_guess == 'paper') or
+      (computer_pick == 'paper' and user_guess == 'spock') or
+      (computer_pick == 'spock' and user_guess == 'rock') or
+      (computer_pick == 'rock' and user_guess == 'scissors')):
+  print('You lose! Computer chose ' + computer_pick + ', and you chose ' + user_guess + '.')
+
+#if player wins
+else:
+  print('You win! Computer chose ' + computer_pick + ', and you chose ' + user_guess + '.') 
