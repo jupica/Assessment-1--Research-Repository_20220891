50bddf11eaabef231cb584792e16dd293f19cb7d
jupica <73052273+jupica@users.noreply.github.com>
2022-08-16 10:59:42 +1200

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,11 +1,12 @@
 import random 
+#create random number
 random_number = random.randint(0,4)
 
 #declare variables
 computer_pick = ''
 user_guess = ''
 
-#computers pick
+#Select Computers Choice Based On The Random Number Generated
 if random_number == 0:
     computer_pick = 'rock'
 elif random_number == 1:
@@ -23,9 +24,10 @@ while (user_guess != 'rock' and
        user_guess != 'scissors' and 
        user_guess != 'lizard' and 
        user_guess != 'spock'):
+    #Make user input lowercase
   user_guess = input('rock, paper, scissors, lizard or spock? ').lower()
 
-#if tie
+#if user and computer choose the same choice
 if computer_pick == user_guess:
     print('Tie! Both chose ' + computer_pick + ', click run to play again.')
 #if computer wins
@@ -39,8 +41,10 @@ elif ((computer_pick == 'scissors' and user_guess == 'paper') or
       (computer_pick == 'paper' and user_guess == 'spock') or
       (computer_pick == 'spock' and user_guess == 'rock') or
       (computer_pick == 'rock' and user_guess == 'scissors')):
+    #print message with the users and computers choice
   print('You lose! Computer chose ' + computer_pick + ', and you chose ' + user_guess + '.')
 
 #if player wins
 else:
+    #print message with the users and computers choice
   print('You win! Computer chose ' + computer_pick + ', and you chose ' + user_guess + '.') 
