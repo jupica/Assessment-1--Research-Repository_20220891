1a2eecfeb4e9f13412af0a37ac5fabef92f709e9
jupica <73052273+jupica@users.noreply.github.com>
2022-08-19 12:32:00 +1200

    Create MadLibs.py

---
+++ MadLibs.py
@@ -0,0 +1,30 @@
+# Asks the user to input a name, and stores it in a variable named "name"
+name = input("Enter a name: ")
+# Asks the user to input an adjective, and stores it in a variable named "adj1"
+adj1 = input("Write an adjective: ")
+# Asks the user to input an adjective, and stores it in a variable named "adj2"
+adj2 = input("Write an adjective: ")
+# Asks the user to input an adjective, and stores it in a variable named "adj3"
+adj3 = input("Write an adjective: ")
+# Asks the user to input a verb, and stores it in a variable named "verb"
+verb = input("Input a verb: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+noun1 = input("Write a noun: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+noun2 = input("Write a noun: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+animal = input("An animal: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+food = input("A food: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+fruit = input("A fruit: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+superhero = input("A superhero: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+country = input("A country: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+dessert = input("A dessert: ")
+# Asks the user to input a name, and stores it in a variable named "name"
+year = input("A year: ")
+
+print(f"This morning {name} woke up feeling {adj1}. 'It is going to be a {adj2} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {noun1}, which made all the {fruit}s very {adj3}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.")
