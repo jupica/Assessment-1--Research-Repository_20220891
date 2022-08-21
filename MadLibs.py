31c5466e3701f4ac1660fb426c6d1f4d72598006
jupica <73052273+jupica@users.noreply.github.com>
2022-08-22 10:55:46 +1200

    Update MadLibs.py

--- MadLibs.py
+++ MadLibs.py
@@ -1,30 +1,44 @@
-# Asks the user to input a name, and stores it in a variable named "name"
+"""Python Mad Libs"""
+"""Mad Libs require a short story with blank spaces (asking for different types of words). Words from the player will fill in those blanks."""
+
+# The template for the story
+
+# prints "Here we go!" into console
+print("Here we go!")
+# prints an empty message into console, resulting in a break, for readability reasons
+print()
+
+# Input prompts ask the user to fill in some questions, saving the users input in seperate variables for each question
 name = input("Enter a name: ")
-# Asks the user to input an adjective, and stores it in a variable named "adj1"
-adj1 = input("Write an adjective: ")
-# Asks the user to input an adjective, and stores it in a variable named "adj2"
-adj2 = input("Write an adjective: ")
-# Asks the user to input an adjective, and stores it in a variable named "adj3"
-adj3 = input("Write an adjective: ")
-# Asks the user to input a verb, and stores it in a variable named "verb"
+adjective1 = input("Write an adjective: ")
+adjective2 = input("One more, please: ")
+adjective3 = input("And the last one: ")
 verb = input("Input a verb: ")
-# Asks the user to input a name, and stores it in a variable named "name"
-noun1 = input("Write a noun: ")
-# Asks the user to input a name, and stores it in a variable named "name"
-noun2 = input("Write a noun: ")
-# Asks the user to input a name, and stores it in a variable named "name"
+noun1 = input("and a noun: ")
+noun2 = input("and one more noun, please: ")
+# prints an empty message into console, resulting in a break, for readability reasons
+print()
+
+# prints a message to the console prompting the user to continue
+print("This is where the story can get really fun!")
+# prints a message to the console, reminding the user what to do
+print("Input one of each of the following, please")
+
+# prints an empty message into console, resulting in a break, for readability reasons
+print()
+# displays prompts asking them to continue inputing values for each input prompt, and stores the users input in seperate variables
 animal = input("An animal: ")
-# Asks the user to input a name, and stores it in a variable named "name"
 food = input("A food: ")
-# Asks the user to input a name, and stores it in a variable named "name"
 fruit = input("A fruit: ")
-# Asks the user to input a name, and stores it in a variable named "name"
 superhero = input("A superhero: ")
-# Asks the user to input a name, and stores it in a variable named "name"
 country = input("A country: ")
-# Asks the user to input a name, and stores it in a variable named "name"
 dessert = input("A dessert: ")
-# Asks the user to input a name, and stores it in a variable named "name"
 year = input("A year: ")
 
-print(f"This morning {name} woke up feeling {adj1}. 'It is going to be a {adj2} day!' Outside, a bunch of {animal}s were protesting to keep {food} in stores. They began to {verb} to the rhythm of the {noun1}, which made all the {fruit}s very {adj3}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.")
+# defines the variable "STORY", which will be printed out. Each "%s" will be replaced with the value of a variable defined by the users input above
+STORY = "This morning %s woke up feeling %s. 'It is going to be a %s day!' Outside, a bunch of %ss were protesting to keep %s in stores. They began to %s to the rhythm of the %s, which made all the %ss very %s. Concerned, %s texted %s, who flew %s to %s and dropped %s in a puddle of frozen %s. %s woke up in the year %s, in a world where %ss ruled the world."
+
+# prints an empty message into console, resulting in a break, for readability reasons
+print()
+# prints the "STORY", replacing the first "%s" with the value of the variable "name", the second "%s" with the value for "adjective1", etc
+print(STORY % (name, adjective1, adjective2, animal, food, verb, noun1, fruit, adjective3, name, superhero, name, country, name, dessert, name, year, noun2))
